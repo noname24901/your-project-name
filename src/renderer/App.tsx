18e890ddfc4961ed57928112e5727aac64ed13cf
dalbeee <acapelles@gmail.com>
2024-01-01 14:26:32 +0900

    base

---
+++ src/renderer/App.tsx
@@ -0,0 +1,60 @@
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import icon from '../../assets/icon.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function Hello() {
+  const [message, setMessage] = useState('none');
+
+  useEffect(() => {
+    window.electron.ipcRenderer.on('message', (message) => {
+      console.log('message', message);
+      setMessage(message);
+    });
+  }, []);
+  return (
+    <div>
+      <div className="Hello">
+        <img width="200" alt="icon" src={icon} />
+      </div>
+      <h1>electron-react-boilerplate</h1>
+      <h2>{message}</h2>
+      <div className="Hello">
+        <a
+          href="https://electron-react-boilerplate.js.org/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button type="button">
+            <span role="img" aria-label="books">
+              📚
+            </span>
+            Read our docs
+          </button>
+        </a>
+        <a
+          href="https://github.com/sponsors/electron-react-boilerplate"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button type="button">
+            <span role="img" aria-label="folded hands">
+              🙏
+            </span>
+            Donate
+          </button>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hello />} />
+      </Routes>
+    </Router>
+  );
+}
